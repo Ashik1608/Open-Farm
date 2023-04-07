56ad746e236218adcf86fd472404690b57dd0239
Luxon C L <luxoncl1234@gmail.com>
2023-04-08 03:13:59 +0530

    feat: update post page

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -2,12 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import BaseLayout from "@/components/BaseLayout";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import { useState } from "react";
+import { MDBBtn } from "mdb-react-ui-kit";
 
 export default function Post() {
-    const [isOpen, setIsOpen] = useState(false);
-  return( 
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDetails, setIsDetails] = useState(0);
+  const [desc, setDesc] = useState({ descrip: "" });
+  return (
     <>
       <Head>
         <title>Open Farm | Post</title>
@@ -17,20 +20,132 @@ export default function Post() {
       </Head>
       <main>
         <BaseLayout>
-        <div className="post_body">
-        <h1 className="post_header"> SELECT ROLES </h1>
-         <div div className="post_select">     
-         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-         </div>
-         <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} >
-            <h1>Modal Content</h1>
-            <button onClick={() => setIsOpen(false)}>Close Modal</button>
-         </Modal>
-      </div>
+          <div className="post_body">
+            <h1 className="post_header"> SELECT ROLES </h1>
+            <div div className="post_select">
+              <button
+                onClick={() => {
+                  setIsDetails(1);
+                  setIsOpen(true);
+                }}
+              >
+                {" "}
+                Land Owner
+              </button>
+              <button
+                onClick={() => {
+                  setIsDetails(2);
+                  setIsOpen(true);
+                }}
+              >
+                Cultivator
+              </button>
+              <button
+                onClick={() => {
+                  setIsDetails(3);
+                  setIsOpen(true);
+                }}
+              >
+                {" "}
+                soil tester
+              </button>
+              <button
+                onClick={() => {
+                  setIsDetails(4);
+                  setIsOpen(true);
+                }}
+              >
+                {" "}
+                Transporter
+              </button>
+              <Modal
+                className="post_bg"
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+              >
+                {isDetails == 1 ? (
+                  <div class="card name details">
+                    <div class="card-body post">
+                      <form>
+                        <h5 class="card-title">Land Details</h5>
+                        <p class="card-text">
+                          Area :
+                          <input
+                            type="text"
+                            className="input_post"
+                            placeholder=" in square meters"
+                          />
+                        </p>
+                        <p class="card-text">
+                          Image :
+                          <input type="file" className="input_post" />
+                        </p>
+                        <p class="card-text">
+                          Location :
+                          <input type="text" className="input_post" />
+                        </p>
+                        <p class="card-text">
+                          Status :
+                          <select className="input_post">
+                            <option value="Start">Starting Stage</option>
+                            <option value="tested">soil Tested</option>
+                            <option value="cultivate">cultivated</option>
+                            <option value="harvest">Harvested</option>
+                            <option value="store">stored</option>
+                          </select>
+                        </p>
+                        <p class="card-text">
+                          Requirement :
+                          <select className="input_post">
+                            <option value="tester">soil Tester</option>
+                            <option value="cultivater">
+                              cultivater/harvester
+                            </option>
+                            <option value="storer">storage Unit</option>
+                            <option value="transport">Transport</option>
+                          </select>
+                        </p>
+                        <p class="card-text">
+                          Description:
+                          <br />
+                          <textarea
+                            className="input_post"
+                            name="desc"
+                            rows="4"
+                            cols="50"
+                            onChange={(event) => {
+                              setDesc({ ...desc, descrip: event.target.value });
+                            }}
+                            value={desc.descrip}
+                          >
+                            {" "}
+                          </textarea>
+                        </p>
+
+                        <button type="button" class="btn btn-primary_login ">
+                          POST
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                ) : isDetails == 2 ? (
+                  <h1> gtgt</h1>
+                ) : isDetails == 3 ? (
+                  <h1> kbkb</h1>
+                ) : isDetails == 4 ? (
+                  <h1> k k</h1>
+                ) : null}
+
+                <button className="close" onClick={() => setIsOpen(false)}>
+                  <MDBBtn
+                    className="btn-close"
+                    color="none"
+                    aria-label="Close"
+                  />
+                </button>
+              </Modal>
+            </div>
+          </div>
         </BaseLayout>
       </main>
     </>
